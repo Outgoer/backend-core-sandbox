805b8558ed990259bb2ad244c9d8dbd6dffd9675
Big Ralph <mr.uwaifo@gmail.com>
2024-01-01 13:38:58 +0100

    added seeders

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        // Using the factory to create 10 posts
+        Post::factory()->count(10)->create();
+    }
+}
